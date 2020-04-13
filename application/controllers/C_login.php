f5caf7feb964ab959657d8f458f61378e94087e9
yahdi <yahdialmukaram03@gmail.com>
2020-04-13 07:07:27 +0700

    perbaikan login

--- application/controllers/C_login.php
+++ application/controllers/C_login.php
@@ -16,31 +16,7 @@ class C_login extends CI_Controller
 
     }
 
-    public function aksi_login()
-    {
-        $username = $this->input->post('username');
-        $password = $this->input->post('password');
-        $where = array(
-            'username' => $username,
-            'password' => md5($password),
-        );
-        $cek = $this->m_login->cek_login("tb_admin", $where)->num_rows();
-        if ($cek > 0) {
-
-            $data_session = array(
-                'nama' => $username,
-                'status' => "login",
-            );
-
-            $this->session->set_userdata($data_session);
-
-            redirect('C_login');
-
-        } else {
-
-            echo "Username dan password salah !";
-        }
-    }
+ 
 
     public function logout()
     {
